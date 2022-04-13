527fb234b1dd5a873c93c26bbf3b01124c20a4bb
unknown <lidanyang1111@gmail.com>
2022-04-13 08:44:59 -0700

    installation-cypress

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,19 @@
 // write tests here
+describe("Quotes App", () => {
+    beforeEach(()=> {
+        cy.visit("http://localhost:1234");
+    })
+})
+
+const textInput = () => cy.get("input[name=text]");
+const authorInput = () => cy.get("input[name=author]");
+const foobarInput = () => cy.get("input[name=foobar]");
+const submitBtn = () => cy.get("button[id='submitBtn']");
+const cancelBtn = () => cy.get("button[id='cancelBtn']");
+
+it("Sanity check to make sure tests work", () => {
+    expect(1 + 2).to.equal(3);
+    expect(2 + 2).to.equal(5);
+    expect({}).not.to.equal({});
+    expect({}).to.eql({});
+})
